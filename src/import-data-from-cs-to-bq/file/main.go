87acbb25938c3ba5f25acf4f49e356a1bf419339
shushuTona <42406967+shushuTona@users.noreply.github.com>
2023-11-23 01:50:03 +0900

    update go file

--- src/import-data-from-cs-to-bq/file/main.go
+++ src/import-data-from-cs-to-bq/file/main.go
@@ -1,24 +1,75 @@
 package insertcsvdatafunction
 
 import (
-    "context"
+	"context"
 	"fmt"
+	"io/ioutil"
 
-    "github.com/GoogleCloudPlatform/functions-framework-go/functions"
-    "github.com/cloudevents/sdk-go/v2/event"
+	"cloud.google.com/go/storage"
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/cloudevents/sdk-go/v2/event"
 )
 
 func init() {
-    // Register a CloudEvent function with the Functions Framework
-    functions.CloudEvent("cloudEventFunction", cloudEventFunction)
+	// Register a CloudEvent function with the Functions Framework
+	functions.CloudEvent("cloudEventFunction", cloudEventFunction)
+}
+
+type EventData struct {
+	Bucket                  string `json:"bucket"`
+	ContentType             string `json:"contentType"`
+	Crc32c                  string `json:"crc32c"`
+	Etag                    string `json:"etag"`
+	Generation              string `json:"generation"`
+	Id                      string `json:"id"`
+	Kind                    string `json:"kind"`
+	Md5Hash                 string `json:"md5Hash"`
+	MediaLink               string `json:"mediaLink"`
+	Metageneration          string `json:"metageneration"`
+	Name                    string `json:"name"`
+	SelfLink                string `json:"selfLink"`
+	Size                    string `json:"size"`
+	StorageClass            string `json:"storageClass"`
+	TimeCreated             string `json:"timeCreated"`
+	TimeStorageClassUpdated string `json:"timeStorageClassUpdated"`
+	Updated                 string `json:"updated"`
 }
 
 // Function myCloudEventFunction accepts and handles a CloudEvent object
 func cloudEventFunction(ctx context.Context, e event.Event) error {
-    // Your code here
-    // Access the CloudEvent data payload via e.Data() or e.DataAs(...)
-	fmt.Printf("event.Event : %#v\n", e)
+	fmt.Printf("e : %#v\n", e)
 
-    // Return nil if no error occurred
-    return nil
+	var eventData EventData
+	if err := e.DataAs(&eventData); err != nil {
+		return err
+	}
+
+	fmt.Printf("eventData : %#v\n", eventData)
+	fmt.Printf("eventData.Bucket : %#v\n", eventData.Bucket)
+	fmt.Printf("eventData.Name : %#v\n", eventData.Name)
+
+	bucketName := eventData.Bucket
+	fileName := eventData.Name
+
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return fmt.Errorf("storage.NewClient: %w", err)
+	}
+	defer client.Close()
+
+	bucketHandle := client.Bucket(bucketName)
+	rc, err := bucketHandle.Object(fileName).NewReader(ctx)
+	if err != nil {
+		return err
+	}
+	defer rc.Close()
+
+	slurp, err := ioutil.ReadAll(rc)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(slurp))
+
+	return nil
 }
