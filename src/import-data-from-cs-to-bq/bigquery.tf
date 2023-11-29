74aadd675e19331ae28f604d422bef7aaf452992
shushuTona <42406967+shushuTona@users.noreply.github.com>
2023-11-30 01:43:28 +0900

    add bigquery.tf

--- src/import-data-from-cs-to-bq/bigquery.tf
+++ src/import-data-from-cs-to-bq/bigquery.tf
@@ -1,2 +1,40 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/bigquery_table
-resource "google_bigquery_dataset" "test-dataset" {}
+resource "google_bigquery_dataset" "test_dataset" {
+  dataset_id = "test_dataset"
+  description = "test dataset"
+  location = "asia-northeast1"
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "labels"
+  }
+}
+
+resource "google_bigquery_table" "test_table" {
+  dataset_id = google_bigquery_dataset.test_dataset.dataset_id
+  table_id = "test_table"
+
+  time_partitioning {
+    type = "DAY"
+  }
+
+  labels = {
+    env = "labels"
+  }
+
+  schema = <<EOF
+[
+    {
+        "name": "id",
+        "type": "INTEGER",
+        "mode": "REQUIRED"
+    },
+    {
+        "name": "name",
+        "type": "STRING",
+        "mode": "NULLABLE"
+    }
+]
+EOF
+
+}
