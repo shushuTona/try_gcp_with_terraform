6a18defdfda938216ede502a249b469cdce263c3
shushuTona <42406967+shushuTona@users.noreply.github.com>
2023-12-06 19:43:14 +0900

    add service-account.tf

---
+++ src/import-data-from-cs-to-bq-with-workflows/service-account.tf
@@ -0,0 +1,25 @@
+data "google_project" "project" {
+}
+
+resource "google_service_account" "account" {
+  account_id   = "with-workflowsf-sa"
+  display_name = "service account used importing data to bq with workflows"
+}
+
+resource "google_project_iam_member" "workflowsinvoker" {
+  project = data.google_project.project.project_id
+  role    = "roles/workflows.invoker"
+  member  = "serviceAccount:${google_service_account.account.email}"
+}
+
+resource "google_project_iam_member" "eventReceiver" {
+  project    = data.google_project.project.project_id
+  role       = "roles/eventarc.eventReceiver"
+  member     = "serviceAccount:${google_service_account.account.email}"
+}
+
+resource "google_project_iam_member" "logWriter" {
+  project    = data.google_project.project.project_id
+  role       = "roles/logging.logWriter"
+  member     = "serviceAccount:${google_service_account.account.email}"
+}
