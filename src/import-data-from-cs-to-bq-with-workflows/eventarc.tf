20988a6e1735dc5048ecbe5d6f34716f3846645e
shushuTona <42406967+shushuTona@users.noreply.github.com>
2023-12-06 19:45:10 +0900

    add import-data-from-cs-to-bq-with-workflows\eventarc.tf

---
+++ src/import-data-from-cs-to-bq-with-workflows/eventarc.tf
@@ -0,0 +1,22 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/eventarc_trigger
+resource "google_eventarc_trigger" "event-trigger" {
+  name = "workflows-trriger-by-cs"
+  location = "asia"
+  # location = "asia-northeast1"
+
+  matching_criteria {
+    attribute = "type"
+    value     = "google.cloud.storage.object.v1.finalized"
+  }
+
+  matching_criteria {
+    attribute = "bucket"
+    value     = google_storage_bucket.csv-bucket.name
+  }
+
+  destination {
+    workflow = google_workflows_workflow.test-workflow.id
+  }
+
+  service_account = google_service_account.account.id
+}
