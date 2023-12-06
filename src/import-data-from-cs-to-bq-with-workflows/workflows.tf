67e7222ae469324dd2ae3b39749913ef67138602
shushuTona <42406967+shushuTona@users.noreply.github.com>
2023-12-06 19:44:27 +0900

    add import-data-from-cs-to-bq-with-workflows\workflows.tf and workflow.yaml

---
+++ src/import-data-from-cs-to-bq-with-workflows/workflows.tf
@@ -0,0 +1,9 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/workflows_workflow
+resource "google_workflows_workflow" "test-workflow" {
+  name          = "workflow"
+  region        = "asia-northeast1"
+  description   = "workflow for importing csv file data to bq"
+  service_account = google_service_account.account.id
+
+  source_contents = file("${path.module}/workflow.yaml")
+}
