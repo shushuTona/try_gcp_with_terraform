881e5d18b820cd767e295427a3cda7f8b3b18d03
shushuTona <42406967+shushuTona@users.noreply.github.com>
2023-12-06 10:53:38 +0900

    add import-data-from-cs-to-bq-with-workflows\bigquery.tf and schema.json

---
+++ src/import-data-from-cs-to-bq-with-workflows/bigquery.tf
@@ -0,0 +1,18 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/bigquery_dataset
+resource "google_bigquery_dataset" "test-dataset" {
+  dataset_id                  = "import_data_from_cs_to_bq_with_workflows"
+  location = "asia-northeast1"
+  default_table_expiration_ms = 3600000
+}
+
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/bigquery_table
+resource "google_bigquery_table" "test-table" {
+  dataset_id = google_bigquery_dataset.test-dataset.dataset_id
+  table_id   = "test_table"
+
+  time_partitioning {
+    type = "DAY"
+  }
+
+  schema = file("${path.module}/schema.json")
+}
