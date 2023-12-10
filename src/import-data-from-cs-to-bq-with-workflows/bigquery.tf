93f2f97378bc51206c54f5e56d16ea46e825c471
shushuTona <42406967+shushuTona@users.noreply.github.com>
2023-12-10 17:56:42 +0900

    remove bigquery dataset default_table_expiration_ms

--- src/import-data-from-cs-to-bq-with-workflows/bigquery.tf
+++ src/import-data-from-cs-to-bq-with-workflows/bigquery.tf
@@ -2,7 +2,6 @@
 resource "google_bigquery_dataset" "test-dataset" {
   dataset_id                  = "import_data_from_cs_to_bq_with_workflows"
   location                    = "asia-northeast1"
-  default_table_expiration_ms = 3600000
 }
 
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/bigquery_table
